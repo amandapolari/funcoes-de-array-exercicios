ce3307342b18e1e025fecb962c40f8564f2027fe
Amanda <amandaoliveirapolari@gmail.com>
2023-03-14 16:14:51 -0300

    Ex 1 - ok

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,34 @@
+// Meu objeto:
 const objeto = {
-    nome: "astrodev",
-    profissao: "Dev das estrelas",
-    username: "astrodev_labenu",
-    senha: "melhorDeTodos"
-}
+    nome: 'astrodev',
+    profissao: 'Dev das estrelas',
+    username: 'astrodev_labenu',
+    senha: 'melhorDeTodos',
+};
+
+// 1)
+const letrasEmCaixaAlta = (item) => {
+    item = { ...objeto };
+    item.nome = item.nome.toUpperCase();
+    item.profissao = item.profissao.toUpperCase();
+    item.username = item.username.toUpperCase();
+    item.senha = item.senha.toUpperCase();
+    return item;
+};
+// console.log(letrasEmCaixaAlta(objeto));
+
+// 2)
+const mensagemCorrida = (item) => {
+    const msg = `O nome é ${item.nome}, a profissão é ${item.profissao}, o username é ${item.username} e a senha é ${item.senha}.`;
+    return msg;
+};
+// console.log(mensagemCorrida(objeto));
+
+// 3)
+const aplicaFuncionalidade = (objetoQualquer, funcaoCallback) => {
+    console.log(funcaoCallback(objetoQualquer));
+};
+
+// Chamadas:
+aplicaFuncionalidade(objeto, letrasEmCaixaAlta);
+aplicaFuncionalidade(objeto, mensagemCorrida);
